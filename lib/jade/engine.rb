775d0b7fa5b347dbb1acdd99874ef9e3047fd2f0
Travis Tilley <ttilley@gmail.com>
2014-04-14 19:36:41 -0400

    remove check against non-existent setting

--- lib/jade/engine.rb
+++ lib/jade/engine.rb
@@ -4,7 +4,6 @@ require 'sprockets/engines'
 module Jade
   class Engine < Rails::Engine
     initializer "jade.configure_rails_initialization", :before => 'sprockets.environment', :group => :all do |app|
-      next unless app.config.assets.enabled
       Sprockets.register_engine '.jade', ::Jade::Template
     end
   end
