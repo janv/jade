d2098ea52c4cfc35a3853d935b99d07453079181
Travis Tilley <ttilley@gmail.com>
2014-04-14 20:43:59 -0400

    register engine with sprockets earlier
    jade templates will not be found if engines aren't set up -before- the "trail" object is configured with extension names

--- lib/jade/engine.rb
+++ lib/jade/engine.rb
@@ -1,9 +1,9 @@
-require 'sprockets'
-require 'sprockets/engines'
+require 'sprockets/railtie'
 
 module Jade
-  class Engine < Rails::Engine
-    initializer "jade.configure_rails_initialization", :before => 'sprockets.environment', :group => :all do |app|
+  class Engine < ::Rails::Engine
+    config.before_initialize do |app|
+      Sprockets::Engines # force autoloading
       Sprockets.register_engine '.jade', ::Jade::Template
     end
   end
